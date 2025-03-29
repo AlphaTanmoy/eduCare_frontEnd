a632fd470d56210aa47ec2267c7e6f4f6f220915
Bhaskar Nandy <bhaskarndy@gmail.com>
2025-03-29 13:15:10 +0530

    Added reset functionality

--- src/app/common-component/custom-image-cropper/custom-image-cropper.component.ts
+++ src/app/common-component/custom-image-cropper/custom-image-cropper.component.ts
@@ -17,28 +17,35 @@ export class CustomImageCropperComponent {
 
   constructor(
     private sanitizer: DomSanitizer
-  ) {
-  }
+  ) { }
 
   fileChangeEvent(event: Event): void {
     this.imageChangedEvent = event;
     this.displayProperty = true;
   }
-  
+
   imageCropped(event: ImageCroppedEvent) {
     if (event.objectUrl) {
-      this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(event.objectUrl);   
+      this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(event.objectUrl);
     } else {
       this.croppedImage = null;
     }
   }
+
   imageLoaded(image: LoadedImage) {
     // show cropper
   }
+
   cropperReady() {
     // cropper ready
   }
+
   loadImageFailed() {
     // show message
+  }
+
+  resetCroppedImage(){
+    this.croppedImage = null;
+    this.displayProperty = false;
   }
 }
