230639c7f8c6681ce1afc8fd1da125a512692d38
Bhaskar Nandy <bhaskarndy@gmail.com>
2025-03-31 00:09:00 +0530

    Updated a minor bug

--- src/app/page-component/admin-panel/update-dashboard-details/home-slideshow/home-slideshow.component.ts
+++ src/app/page-component/admin-panel/update-dashboard-details/home-slideshow/home-slideshow.component.ts
@@ -63,7 +63,7 @@ export class HomeSlideshowComponent {
             });
           });
 
-          this.hideMatProgressBar();
+          if(this.images.length === 0) this.hideMatProgressBar();
         } catch (error) {
           console.error("Error processing images:", error);
           this.hideMatProgressBar();
