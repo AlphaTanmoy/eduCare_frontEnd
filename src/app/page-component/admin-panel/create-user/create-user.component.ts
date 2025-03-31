e130b7dd33bc6edfa62d7d7d7f6d5562cd9c73d4
Bhaskar Nandy <bhaskarndy@gmail.com>
2025-03-31 23:43:16 +0530

    Added comments in custom components

--- src/app/page-component/admin-panel/create-user/create-user.component.ts
+++ src/app/page-component/admin-panel/create-user/create-user.component.ts
@@ -1,45 +1,21 @@
-import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { loadBootstrap, removeBootstrap } from '../../../../load-bootstrap';
-import { MatDialog } from '@angular/material/dialog';
-import { CustomSingleSelectSearchableDropdownComponent } from '../../../common-component/custom-single-select-searchable-dropdown/custom-single-select-searchable-dropdown.component';
-import { CustomAlertComponent } from '../../../common-component/custom-alert/custom-alert.component';
-import { ResponseTypeColor } from '../../../constants/commonConstants';
 
 @Component({
   selector: 'app-create-user',
-  imports: [CustomSingleSelectSearchableDropdownComponent],
+  imports: [],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css'
 })
 
 export class CreateUserComponent implements OnInit, OnDestroy {
   private bootstrapElements!: { css: HTMLLinkElement; js: HTMLScriptElement };
-  dropdownOption: string[] = ['One', 'Two', 'Three', 'Four'];
-  dropdownplaceholder: string = "Search/Select an item";
-  dropdownpLabel: string = "Number selection";
 
   ngOnInit(): void {
     this.bootstrapElements = loadBootstrap();
-    this.openDialog();
   }
 
   ngOnDestroy(): void {
     removeBootstrap(this.bootstrapElements);
-  }
-
-  handleOptionSelection(event: any): void {
-    console.log(event, " is selected");
-  }
-
-  readonly dialog = inject(MatDialog);
-
-  openDialog() {
-    const dialogRef = this.dialog.open(CustomAlertComponent, {
-      data: {
-        title: 'Custom Dialog Title',
-        text: 'This is a dynamically passed dialog message.',
-        type: ResponseTypeColor.SUCCESS
-      }
-    });
   }
 }
