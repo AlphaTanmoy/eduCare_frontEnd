19cb0c79a42401ca05ed502fa68da7c32eafe4c8
alphaTanmoy <dtanmoy169@gmail.com>
2025-03-31 11:42:03 +0530

    appcomponent updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,29 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons';
 import { VerticalLayoutComponent } from './layout/vertical-layout/vertical-layout.component';
 import { HorizontalLayoutComponent } from './layout/horizontal-layout/horizontal-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, HorizontalLayoutComponent, VerticalLayoutComponent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    HorizontalLayoutComponent,
+    VerticalLayoutComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
 export class AppComponent {
   isSmallScreen = window.innerWidth < 1200;
+  faExpandArrowsAlt = faExpandArrowsAlt;
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faExpandArrowsAlt);
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
