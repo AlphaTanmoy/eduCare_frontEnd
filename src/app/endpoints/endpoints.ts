1037b806219cce92d5018ac4f38c4c2938953ccb
alphaTanmoy <dtanmoy169@gmail.com>
2025-03-31 10:49:35 +0530

    endpoints updated

--- src/app/endpoints/endpoints.ts
+++ src/app/endpoints/endpoints.ts
@@ -11,5 +11,10 @@ export const Endpoints = {
     dashboard: {
         get_dashboard_slideshow_images: "dashboard/get_dashboard_slideshow_images",
         get_image_stream_by_id: "dashboard/get_image_stream_by_id",
+    },
+    enums: {
+        get_all_enums: "enums/",
+        get_enums_by_name: "enums/group-by",
+        get_enum_names: "enums/name"
     }
 }
