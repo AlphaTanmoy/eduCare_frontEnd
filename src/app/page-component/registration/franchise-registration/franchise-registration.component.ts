e76fae5086440521ed075fc3ece17397e3e7439e
alphaTanmoy <dtanmoy169@gmail.com>
2025-04-01 20:49:28 +0530

    franchise added

---
+++ src/app/page-component/registration/franchise-registration/franchise-registration.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { loadBootstrap, removeBootstrap } from '../../../../load-bootstrap';
+import { faUserPlus, faUserTag, faPhone, faHouseUser, faMailForward,faHotel, faSitemap, faGlobe, faHome, faSearchLocation, faLocationPinLock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+@Component({
+  selector: 'app-franchise-registration',
+  imports: [FontAwesomeModule],
+  templateUrl: './franchise-registration.component.html',
+  styleUrl: './franchise-registration.component.css'
+})
+
+
+export class FranchiseRegistrationComponent {
+
+  private bootstrapElements!: { css: HTMLLinkElement; js: HTMLScriptElement };
+  faUserPlus = faUserPlus;
+  faUserTag= faUserTag;
+  faPhone = faPhone;
+  faSitemap = faSitemap;
+  faMailForward = faMailForward;
+  faGlobe = faGlobe;
+  faSearchLocation = faSearchLocation;
+  faLocationPinLock = faLocationPinLock;
+  faHome = faHome;
+  faHotel = faHotel;
+  faHouseUser = faHouseUser;
+  ngOnInit(): void {
+    this.bootstrapElements = loadBootstrap();
+  }
+
+  ngOnDestroy(): void {
+    removeBootstrap(this.bootstrapElements);
+  }
+}
