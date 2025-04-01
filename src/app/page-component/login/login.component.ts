49b03f792eb404a74617c0076b23bb823c7c6b9b
bhaskar4k <bhaskarndy@gmail.com>
2025-04-01 23:49:38 +0530

    Added bootstrap in login component

--- src/app/page-component/login/login.component.ts
+++ src/app/page-component/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { loadBootstrap, removeBootstrap } from '../../../load-bootstrap';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private bootstrapElements!: { css: HTMLLinkElement; js: HTMLScriptElement };
 
+  ngOnInit(): void {
+    this.bootstrapElements = loadBootstrap();
+  }
+
+  ngOnDestroy(): void {
+    removeBootstrap(this.bootstrapElements);
+  }
 }
