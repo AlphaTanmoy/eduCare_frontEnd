ba7621806432f9e6316d259f2f5cc73ba98ed5f8
bhaskar4k <bhaskarndy@gmail.com>
2025-04-02 23:10:45 +0530

    Updated menuConstants

--- src/app/constants/menuConstants.ts
+++ src/app/constants/menuConstants.ts
@@ -13,19 +13,16 @@ export const MenuItems: any[] = [
       {
         id: 21,
         name: 'Certificate Verify',
-        visible: true,
         subMenu: [
           {
             id: 211,
             name: 'Course Certificate Verify',
             route: '/verification/certificate/course',
-            visible: true,
           },
           {
             id: 212,
             name: 'Other Certificate Verify',
             route: '/verification/certificate/other',
-            visible: true,
           },
         ],
       },
@@ -33,13 +30,11 @@ export const MenuItems: any[] = [
         id: 22,
         name: 'Student Verify',
         route: '/verification/student',
-        visible: true,
       },
       {
         id: 23,
         name: 'Center Verify',
         route: '/verification/center',
-        visible: true,
       },
     ],
   },
@@ -52,25 +47,21 @@ export const MenuItems: any[] = [
         id: 31,
         name: 'Download Certificate',
         route: 'academic/download-certificate',
-        visible: true,
       },
       {
         id: 32,
         name: 'Download Form',
         route: '/academic/download-form',
-        visible: true,
       },
       {
         id: 33,
         name: 'Pay Fees',
         route: '/academic/pay-fees',
-        visible: true,
       },
       {
         id: 34,
         name: 'Apply Franchies',
         route: '/registration/register-franchise',
-        visible: true,
       },
     ],
   },
@@ -83,31 +74,26 @@ export const MenuItems: any[] = [
         id: 41,
         name: 'Computer',
         route: '/course-offered/computer',
-        visible: true,
       },
       {
         id: 42,
         name: 'Drawing',
         route: '/course-offered/drawing',
-        visible: true,
       },
       {
         id: 43,
         name: 'Dance',
         route: '/course-offered/dance',
-        visible: true,
       },
       {
         id: 44,
         name: 'Spoken English',
         route: '/course-offered/drawing',
-        visible: true,
       },
       {
         id: 45,
         name: 'Others',
         route: '/course-offered/others',
-        visible: true,
       },
     ],
   },
@@ -119,25 +105,21 @@ export const MenuItems: any[] = [
       {
         id: 51,
         name: 'Notice',
-        visible: true,
         subMenu: [
           {
             id: 511,
             name: 'Reg Notice',
             route: '/student/notice/registration',
-            visible: true,
           },
           {
             id: 512,
             name: 'Holiday Notice',
             route: '/student/notice/holiday',
-            visible: true,
           },
           {
             id: 513,
             name: 'Student Login',
             route: '/login',
-            visible: true,
           },
         ],
       },
@@ -145,31 +127,26 @@ export const MenuItems: any[] = [
         id: 52,
         name: 'E-Book',
         route: '/student/e-book',
-        visible: true,
       },
       {
         id: 53,
         name: 'Notes',
         route: '/student/notes',
-        visible: true,
       },
       {
         id: 54,
         name: 'Marks Division',
         route: '/student/marks-division',
-        visible: true,
       },
       {
         id: 55,
         name: 'Software Download',
         route: '/student/software-download',
-        visible: true,
       },
       {
         id: 56,
         name: 'Student Registration',
         route: '/registration/register-student',
-        visible: true,
       }
     ],
   },
@@ -182,25 +159,21 @@ export const MenuItems: any[] = [
         id: 61,
         name: 'Photos - 1',
         route: '/gallary/1',
-        visible: true,
       },
       {
         id: 62,
         name: 'Photos - 2',
         route: '/gallary/2',
-        visible: true,
       },
       {
         id: 63,
         name: 'Photos - 3',
         route: '/gallary/3',
-        visible: true,
       },
       {
         id: 64,
         name: 'Others Activity',
         route: '/gallary/others-activity',
-        visible: true,
       },
     ],
   },
@@ -213,13 +186,11 @@ export const MenuItems: any[] = [
         id: 71,
         name: 'About Institution',
         route: '/about/institution',
-        visible: true,
       },
       {
         id: 72,
         name: 'About Registration',
         route: '/about/registration',
-        visible: true,
       },
     ],
   },
@@ -232,19 +203,16 @@ export const MenuItems: any[] = [
         id: 81,
         name: 'Contact Us',
         route: '/contact-us/contact',
-        visible: true,
       },
       {
         id: 82,
         name: 'Feedback',
         route: '/contact-us/feedback',
-        visible: true,
       },
       {
         id: 83,
         name: 'Rules',
         route: '/contact-us/rules',
-        visible: true,
       },
     ],
   },
@@ -256,13 +224,11 @@ export const MenuItems: any[] = [
       {
         id: 91,
         name: 'Update Dashboard Details',
-        visible: true,
         subMenu: [
           {
             id: 911,
             name: 'Home Slideshow',
             route: '/admin-panel/update-dashboard-details/home-slideshow',
-            visible: true,
           },
         ],
       },
@@ -270,13 +236,11 @@ export const MenuItems: any[] = [
         id: 92,
         name: 'Create User',
         route: '/admin-panel/create-user',
-        visible: true,
       },
       {
         id: 93,
         name: 'Enums',
         route: '/admin-panel/enum',
-        visible: true,
       },
     ],
   },
@@ -289,6 +253,7 @@ export const MenuItems: any[] = [
   {
     id: 11,
     name: 'User',
+    showWhenLoggedIn: true,
     subMenu: [
       {
         id: 111,
@@ -301,6 +266,5 @@ export const MenuItems: any[] = [
         action: 'logout',
       },
     ],
-    showWhenLoggedIn: true
   },
 ];
