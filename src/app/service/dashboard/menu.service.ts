0e137544afc71b628df5237b7c69f23314c69bef
alphaTanmoy <dtanmoy169@gmail.com>
2025-04-02 21:23:24 +0530

    menu updated

---
+++ src/app/service/dashboard/menu.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { MenuItems } from '../../constants/menuConstants'; // Import menu from the file
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+  constructor() {}
+
+  getMenuItems(): Observable<any[]> {
+    return of(MenuItems); // Return the menu as an observable
+  }
+}
