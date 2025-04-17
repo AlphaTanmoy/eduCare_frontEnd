eef81b0f3e1bb6f5226edf2c02d0e1b0d00b9012
Bhaskar Nandy <bhaskarndy@gmail.com>
2025-04-17 18:46:35 +0530

    Updated multi select dropdown

--- src/app/common-component/custom-multi-select-dropdown/custom-multi-select-dropdown.component.ts
+++ src/app/common-component/custom-multi-select-dropdown/custom-multi-select-dropdown.component.ts
@@ -1,22 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-custom-multi-select-dropdown',
-  imports: [MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule],
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './custom-multi-select-dropdown.component.html',
   styleUrl: './custom-multi-select-dropdown.component.css'
 })
 export class CustomMultiSelectDropdownComponent {
-  toppings = new FormControl('');
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+  selectedItems = new FormControl('');
+  itemList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 
+  @Input() dropdownLabel: string = "";
   @Output() selectedItemsChanged = new EventEmitter<string[]>();
 
   constructor() {
-    this.toppings.valueChanges.subscribe((value: any) => {
+    this.selectedItems.valueChanges.subscribe((value: any) => {
       this.selectedItemsChanged.emit(value);
     });
   }
