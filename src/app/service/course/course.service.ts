bd2dc3429ec5b7f920724cf487452d49537cc7c2
alphaTanmoy <dtanmoy169@gmail.com>
2025-04-20 16:29:47 +0530

    course service added

---
+++ src/app/service/course/course.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GetBaseURL, Endpoints } from '../../endpoints/endpoints';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+  constructor(private http: HttpClient) {}
+
+  getAllParentCategories(): Observable<any> {
+    return this.http.get(GetBaseURL() + Endpoints.course.get_all_parent_categories);
+  }
+
+  addParentCategory(courseName: string): Observable<any> {
+    return this.http.post(GetBaseURL() + Endpoints.course.add_parent_category, {
+      course_name: courseName
+    });
+  }
+
+  addSubCategory(
+    parentCourseId: string,
+    courseName: string,
+    duration: string,
+    module: string,
+    moduleDetails: string[][]
+  ): Observable<any> {
+    return this.http.post(GetBaseURL() + Endpoints.course.add_sub_category, {
+      parentCourseId,
+      course_name: courseName,
+      duration,
+      module,
+      module_details: moduleDetails
+    });
+  }
+
+  editParentCategory(courseCode: string, courseName: string): Observable<any> {
+    return this.http.put(GetBaseURL() + Endpoints.course.edit_parent_category, {
+      courseCode,
+      course_name: courseName
+    });
+  }
+
+  editSubCategory(
+    courseCode: string,
+    courseName: string,
+    duration: string,
+    module: string,
+    moduleDetails: string[][]
+  ): Observable<any> {
+    return this.http.put(GetBaseURL() + Endpoints.course.edit_sub_category, {
+      courseCode,
+      course_name: courseName,
+      duration,
+      module,
+      module_details: moduleDetails
+    });
+  }
+
+  deleteParentCategory(courseCode: string): Observable<any> {
+    return this.http.delete(GetBaseURL() + Endpoints.course.delete_parent_category + `/${courseCode}`);
+  }
+
+  deleteSubCategory(courseCode: string): Observable<any> {
+    return this.http.delete(GetBaseURL() + Endpoints.course.delete_sub_category + `/${courseCode}`);
+  }
+} 
