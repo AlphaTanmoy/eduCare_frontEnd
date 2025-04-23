040cc31956be9228ac02f19ec8fcd37e3798c64a
bhaskar4k <bhaskarndy@gmail.com>
2025-04-23 23:57:54 +0530

    Updated T&C

--- src/app/common-component/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/common-component/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-terms-and-conditions',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './terms-and-conditions.component.html',
   styleUrl: './terms-and-conditions.component.css'
 })
 export class TermsAndConditionsComponent {
+  IsChecked: boolean = false;
 
+  onCheckboxChange(event: any){
+
+  }
 }
