3b5948901d136e4a804dc5ab4947263f55788ad7
Bhaskar Nandy <bhaskarndy@gmail.com>
2025-04-30 22:07:38 +0530

    Updated course service

--- src/app/service/course/course.service.ts
+++ src/app/service/course/course.service.ts
@@ -19,118 +19,60 @@ interface EnumValue {
   providedIn: 'root'
 })
 export class CourseService {
-  constructor(
-    private http: HttpClient,
-    private authService: AuthService
-  ) {}
-
-  private getHeaders(): HttpHeaders {
-    const token = this.authService.getToken();
-    return new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    });
-  }
+  constructor(private http: HttpClient) { }
 
   getModuleTypes(): Observable<EnumValue[]> {
-    return this.http.get<EnumValue[]>(
-      `${GetBaseURL()}${Endpoints.enums.get_enums_by_name}/module_type`,
-      { headers: this.getHeaders() }
-    );
+    return this.http.get<EnumValue[]>(`${GetBaseURL()}${Endpoints.enums.get_enums_by_name}/module_type`);
   }
 
   getDurations(): Observable<EnumValue[]> {
-    return this.http.get<EnumValue[]>(
-      `${GetBaseURL()}${Endpoints.enums.get_enums_by_name}/duration_type`,
-      { headers: this.getHeaders() }
-    );
+    return this.http.get<EnumValue[]>(`${GetBaseURL()}${Endpoints.enums.get_enums_by_name}/duration_type`);
   }
 
   getCourseById(id: string): Observable<CourseResponse> {
-    return this.http.get<CourseResponse>(
-      `${GetBaseURL()}${Endpoints.course.get_course}/${id}`,
-      { headers: this.getHeaders() }
-    );
+    return this.http.get<CourseResponse>(`${GetBaseURL()}${Endpoints.course.get_course}/${id}`);
   }
 
   getSubCourseById(id: string): Observable<CourseResponse> {
-    return this.http.get<CourseResponse>(
-      `${GetBaseURL()}sub-category/byId/${id}`,
-      { headers: this.getHeaders() }
-    );
+    return this.http.get<CourseResponse>(`${GetBaseURL()}sub-category/byId/${id}`);
   }
 
   getAllParentCategories(): Observable<any> {
-    return this.http.get(
-      `${GetBaseURL()}${Endpoints.course.get_all_parent_categories}`,
-      { headers: this.getHeaders() }
-    );
+    return this.http.get(`${GetBaseURL()}${Endpoints.course.get_all_parent_categories}`);
   }
 
   addParentCategory(courseName: string): Observable<any> {
-    return this.http.post(
-      `${GetBaseURL()}${Endpoints.course.add_parent_category}`,
-      { course_name: courseName },
-      { headers: this.getHeaders() }
-    );
+    return this.http.post(`${GetBaseURL()}${Endpoints.course.add_parent_category}`, { course_name: courseName });
   }
 
-  addSubCategory(
-    parentCourseId: string,
-    courseName: string,
-    duration: string,
-    module: string,
-    moduleDetails: string[][]
-  ): Observable<any> {
-    return this.http.post(
-      `${GetBaseURL()}${Endpoints.course.add_sub_category}`,
-      {
-        parentCourseId,
-        course_name: courseName,
-        duration,
-        module,
-        module_details: moduleDetails
-      },
-      { headers: this.getHeaders() }
-    );
+  addSubCategory(parentCourseId: string, courseName: string, duration: string, module: string, moduleDetails: string[][]): Observable<any> {
+    var obj = {
+      parentCourseId,
+      course_name: courseName,
+      duration,
+      module,
+      module_details: moduleDetails
+    }
+    return this.http.post(`${GetBaseURL()}${Endpoints.course.add_sub_category}`, obj);
   }
 
   editParentCategory(courseCode: string, courseName: string): Observable<any> {
-    return this.http.put(
-      `${GetBaseURL()}${Endpoints.course.edit_parent_category}`,
-      {
-        courseCode,
-        course_name: courseName
-      },
-      { headers: this.getHeaders() }
-    );
+    var obj = {
+      courseCode,
+      course_name: courseName
+    }
+    return this.http.put(`${GetBaseURL()}${Endpoints.course.edit_parent_category}`, obj);
   }
 
-  editSubCategory(requestBody: {
-    id: string;
-    course_code: string;
-    duration: string;
-    module: string;
-    module_details: string[][];
-  }): Observable<any> {
-    return this.http.put(
-      `${GetBaseURL()}${Endpoints.course.edit_sub_category}`,
-      requestBody,
-      { headers: this.getHeaders() }
-    );
+  editSubCategory(requestBody: { id: string; course_code: string; duration: string; module: string; module_details: string[][]; }): Observable<any> {
+    return this.http.put(`${GetBaseURL()}${Endpoints.course.edit_sub_category}`, requestBody);
   }
 
   deleteParentCategory(courseCode: string): Observable<any> {
-    return this.http.delete(
-      `${GetBaseURL()}${Endpoints.course.delete_parent_category}/${courseCode}`,
-      { headers: this.getHeaders() }
-    );
+    return this.http.delete(`${GetBaseURL()}${Endpoints.course.delete_parent_category}/${courseCode}`);
   }
 
   deleteSubCategory(courseCode: string): Observable<any> {
-    return this.http.delete(
-      `${GetBaseURL()}${Endpoints.course.delete_sub_category}/${courseCode}`,
-      { headers: this.getHeaders() }
-    );
+    return this.http.delete(`${GetBaseURL()}${Endpoints.course.delete_sub_category}/${courseCode}`);
   }
 }
