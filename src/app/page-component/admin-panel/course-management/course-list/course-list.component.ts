8882f8956fc46e64a1cd2a820770a6061f6e27c1
Bhaskar Nandy <bhaskarndy@gmail.com>
2025-05-02 22:13:47 +0530

    fix

--- src/app/page-component/admin-panel/course-management/course-list/course-list.component.ts
+++ src/app/page-component/admin-panel/course-management/course-list/course-list.component.ts
@@ -30,8 +30,8 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   styleUrls: ['./course-list.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
-      state('expanded', style({ height: '*' })),
+      state('collapsed', style({ height: '0px', minHeight: '0', opacity: '0' })),
+      state('expanded', style({ height: '*', opacity: '1' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
