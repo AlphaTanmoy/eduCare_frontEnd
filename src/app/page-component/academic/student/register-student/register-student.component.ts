9dcff7ea05b5d4ca772c531d32eba9d94ba17d41
bhaskar4k <bhaskarndy@gmail.com>
2025-05-31 20:58:50 +0530

    Updated

--- src/app/page-component/academic/student/register-student/register-student.component.ts
+++ src/app/page-component/academic/student/register-student/register-student.component.ts
@@ -1,17 +1,31 @@
-import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CustomAlertComponent } from '../../../../common-component/custom-alert/custom-alert.component';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { CommonModule } from '@angular/common';
 import { loadBootstrap, removeBootstrap } from '../../../../../load-bootstrap';
 import { CustomDatePickerComponent } from '../../../../common-component/custom-date-picker/custom-date-picker.component';
 import { Dropdown, Gender, MaritalStatus } from '../../../../constants/commonConstants';
 import { CustomSingleSelectSearchableDropdownComponent } from '../../../../common-component/custom-single-select-searchable-dropdown/custom-single-select-searchable-dropdown.component';
+import { MatStepperModule } from '@angular/material/stepper';
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
+import { TermsAndConditionsComponent } from '../../../../common-component/terms-and-conditions/terms-and-conditions.component';
 
 @Component({
   selector: 'app-register-student',
-  imports: [CommonModule, FormsModule, MatProgressBarModule, CustomDatePickerComponent, CustomSingleSelectSearchableDropdownComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatProgressBarModule,
+    CustomDatePickerComponent,
+    CustomSingleSelectSearchableDropdownComponent,
+    CommonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TermsAndConditionsComponent
+  ],
   standalone: true,
   templateUrl: './register-student.component.html',
   styleUrl: './register-student.component.css',
@@ -22,8 +36,30 @@ export class RegisterStudentComponent {
   ) { }
 
   private bootstrapElements!: { css: HTMLLinkElement; js: HTMLScriptElement };
+  private _formBuilder = inject(FormBuilder);
   matProgressBarVisible = false;
   readonly dialog = inject(MatDialog);
+
+  selectedStepIndex: number = 0;
+  isLinear = false;
+  stepperOrientation: 'horizontal' | 'vertical' = 'horizontal';
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+
+  secondFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+
+  thirdFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+
+  forthFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+
 
   marital_status_option: Dropdown[] = MaritalStatus;
   gender_option: Dropdown[] = Gender;
@@ -33,9 +69,35 @@ export class RegisterStudentComponent {
   student_DOB: Date | null = null;
   student_maratial_status: string = '';
   student_gender: string = '';
+  student_email: string = '';
+  student_phone_no: string = '';
+  student_whats_app: string = '';
+
+  student_fathers_name: string = '';
+  student_mothers_name: string = '';
+  student_husbands_name: string = '';
+  student_wifes_name: string = '';
+  student_guardians_number: number | null = null;
+
+  student_state: string = '';
+  student_district: string = '';
+  student_post_office: string = '';
+  student_village_city: string = '';
+  student_pincode: string = '';
+
+  terms_and_conditions_status: boolean = false;
 
   ngOnInit() {
     this.bootstrapElements = loadBootstrap();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.setStepperOrientation();
+  }
+
+  onStepChange(event: StepperSelectionEvent): void {
+    this.selectedStepIndex = event.selectedIndex;
   }
 
   onDobSelected(date: Date) {
@@ -51,12 +113,20 @@ export class RegisterStudentComponent {
     this.student_gender = event.text;
   }
 
+  TermsAndConditionStatus(status: boolean) {
+    this.terms_and_conditions_status = status;
+  }
+
   submit() {
 
   }
 
   isValid() {
     return true;
+  }
+
+  setStepperOrientation() {
+    this.stepperOrientation = window.innerWidth < 1200 ? 'vertical' : 'horizontal';
   }
 
   ngOnDestroy(): void {
