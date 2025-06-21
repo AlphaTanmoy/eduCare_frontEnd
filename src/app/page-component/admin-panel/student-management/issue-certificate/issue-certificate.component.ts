0c7bf1bb09a5ddcfb3e43e21c436e9321ac4cb72
bhaskar4k <bhaskarndy@gmail.com>
2025-06-22 03:33:32 +0530

    Created issue certificate component

---
+++ src/app/page-component/admin-panel/student-management/issue-certificate/issue-certificate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-certificate',
+  imports: [],
+  templateUrl: './issue-certificate.component.html',
+  styleUrl: './issue-certificate.component.css'
+})
+export class IssueCertificateComponent {
+
+}
