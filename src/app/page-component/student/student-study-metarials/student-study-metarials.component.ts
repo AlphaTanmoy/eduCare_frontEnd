1f1365903f905c4a0f2cee8b154ea98f37b439a1
alphaTanmoy <dtanmoy169@gmail.com>
2025-06-22 12:43:53 +0530

    studey-metarials added

---
+++ src/app/page-component/student/student-study-metarials/student-study-metarials.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-study-metarials',
+  imports: [],
+  templateUrl: './student-study-metarials.component.html',
+  styleUrl: './student-study-metarials.component.css'
+})
+export class StudentStudyMetarialsComponent {
+
+}
