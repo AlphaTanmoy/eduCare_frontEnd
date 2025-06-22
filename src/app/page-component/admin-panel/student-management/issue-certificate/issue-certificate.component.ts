1bb8649a07cfd33a0bd7b78c7d2cd222b499dded
bhaskar4k <bhaskarndy@gmail.com>
2025-06-22 13:39:23 +0530

    Updated styling

--- src/app/page-component/admin-panel/student-management/issue-certificate/issue-certificate.component.ts
+++ src/app/page-component/admin-panel/student-management/issue-certificate/issue-certificate.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-issue-certificate',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './issue-certificate.component.html',
   styleUrl: './issue-certificate.component.css'
 })
 export class IssueCertificateComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<IssueCertificateComponent>,
+  ) { }
 
+  Save(){
+
+  }
+
+  Close(){
+    this.dialogRef.close();
+  }
 }
