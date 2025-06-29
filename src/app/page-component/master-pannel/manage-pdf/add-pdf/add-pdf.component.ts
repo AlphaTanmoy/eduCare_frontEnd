a722dd90d1c687621f70df20dc9cc61a6572edd7
alphaTanmoy <dtanmoy169@gmail.com>
2025-06-30 00:37:12 +0530

    add-pdf added

---
+++ src/app/page-component/master-pannel/manage-pdf/add-pdf/add-pdf.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-pdf',
+  imports: [],
+  templateUrl: './add-pdf.component.html',
+  styleUrl: './add-pdf.component.css'
+})
+export class AddPdfComponent {
+
+}
