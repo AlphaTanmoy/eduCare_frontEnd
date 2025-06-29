2a555e7a56fadddc2cbf70d159b7dedb1f2f0167
alphaTanmoy <dtanmoy169@gmail.com>
2025-06-30 00:38:34 +0530

    view-brand added

---
+++ src/app/page-component/view-home-brand/view-home-brand.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-home-brand',
+  imports: [],
+  templateUrl: './view-home-brand.component.html',
+  styleUrl: './view-home-brand.component.css'
+})
+export class ViewHomeBrandComponent {
+
+}
