dd6fec442bbfce1a5ae1c89f30aeaecaaa307239
alphaTanmoy <dtanmoy169@gmail.com>
2025-06-29 12:19:07 +0530

    add brands component

---
+++ src/app/page-component/admin-panel/brands-add/brands-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brands-add',
+  imports: [],
+  templateUrl: './brands-add.component.html',
+  styleUrl: './brands-add.component.css'
+})
+export class BrandsAddComponent {
+
+}
