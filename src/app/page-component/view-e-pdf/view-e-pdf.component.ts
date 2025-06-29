b19795a50fb32f6ed84c193892ffdf8fb5a8e82a
alphaTanmoy <dtanmoy169@gmail.com>
2025-06-30 00:37:46 +0530

    view-pdf added

---
+++ src/app/page-component/view-e-pdf/view-e-pdf.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-e-pdf',
+  imports: [],
+  templateUrl: './view-e-pdf.component.html',
+  styleUrl: './view-e-pdf.component.css'
+})
+export class ViewEPdfComponent {
+
+}
