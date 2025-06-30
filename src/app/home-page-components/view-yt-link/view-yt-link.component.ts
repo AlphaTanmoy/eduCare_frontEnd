f32e6fe2b8af0f1cc846d13abf8f3cc7eec0844a
alphaTanmoy <dtanmoy169@gmail.com>
2025-06-30 10:37:40 +0530

    updated

---
+++ src/app/home-page-components/view-yt-link/view-yt-link.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-yt-link',
+  imports: [],
+  templateUrl: './view-yt-link.component.html',
+  styleUrl: './view-yt-link.component.css'
+})
+export class ViewYtLinkComponent {
+
+}
