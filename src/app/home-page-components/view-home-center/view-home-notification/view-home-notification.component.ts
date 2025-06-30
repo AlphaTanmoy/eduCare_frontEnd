611be539960a0a7e4f27228bac1d194f6408d576
alphaTanmoy <dtanmoy169@gmail.com>
2025-06-30 10:37:59 +0530

    updated

---
+++ src/app/home-page-components/view-home-center/view-home-notification/view-home-notification.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-home-notification',
+  imports: [],
+  templateUrl: './view-home-notification.component.html',
+  styleUrl: './view-home-notification.component.css'
+})
+export class ViewHomeNotificationComponent {
+
+}
