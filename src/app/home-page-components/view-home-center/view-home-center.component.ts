0289643f96dfd22dc355913d260f87f896296f4b
alphaTanmoy <dtanmoy169@gmail.com>
2025-06-30 10:38:06 +0530

    updated

---
+++ src/app/home-page-components/view-home-center/view-home-center.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-home-center',
+  imports: [],
+  templateUrl: './view-home-center.component.html',
+  styleUrl: './view-home-center.component.css'
+})
+export class ViewHomeCenterComponent {
+
+}
