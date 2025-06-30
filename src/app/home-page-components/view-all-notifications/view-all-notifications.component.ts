127aff129128c0ebd6df33447afd40f6d3b9b128
alphaTanmoy <dtanmoy169@gmail.com>
2025-06-30 11:35:13 +0530

    all bnotification view added

---
+++ src/app/home-page-components/view-all-notifications/view-all-notifications.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-notifications',
+  imports: [],
+  templateUrl: './view-all-notifications.component.html',
+  styleUrl: './view-all-notifications.component.css'
+})
+export class ViewAllNotificationsComponent {
+
+}
