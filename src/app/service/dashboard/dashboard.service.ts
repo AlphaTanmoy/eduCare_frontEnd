2c456c09eb537cbf88a309ee621e8068bdcd7d8b
alphaTanmoy <dtanmoy169@gmail.com>
2025-06-30 16:17:37 +0530

    updated service

--- src/app/service/dashboard/dashboard.service.ts
+++ src/app/service/dashboard/dashboard.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 import { GetBaseURL, Endpoints } from '../../endpoints/endpoints';
 
 @Injectable({
@@ -8,6 +9,8 @@ import { GetBaseURL, Endpoints } from '../../endpoints/endpoints';
 })
 
 export class DashboardService {
+  private masterDataCache$: Observable<any> | null = null;
+
   constructor(private http: HttpClient) { }
 
   getAllDashboardSlideshowImages(): Observable<any> {
@@ -29,11 +32,17 @@ export class DashboardService {
     return this.http.post<any>(GetBaseURL() + Endpoints.dashboard.delete_dashboard_slideshow_image, { fileId });
   }
 
-  getAllDashboardMasterData(): Observable<any> {
-    return this.http.get<any>(GetBaseURL() + Endpoints.dashboard.get_dashboard_master_data);
+  getAllDashboardMasterData(forceRefresh: boolean = false): Observable<any> {
+    if (!this.masterDataCache$ || forceRefresh) {
+      this.masterDataCache$ = this.http.get<any>(
+        GetBaseURL() + Endpoints.dashboard.get_dashboard_master_data
+      ).pipe(shareReplay(1));
+    }
+    return this.masterDataCache$;
   }
 
   updateDashboardMasterData(data: any): Observable<any> {
+    this.masterDataCache$ = null;
     return this.http.post<any>(GetBaseURL() + Endpoints.dashboard.update_dashboard_master_data, data);
   }
 }
