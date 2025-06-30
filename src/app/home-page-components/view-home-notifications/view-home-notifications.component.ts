2856c4a7b57b1adc2d415bf67d7f6133dfd05ebd
alphaTanmoy <dtanmoy169@gmail.com>
2025-06-30 11:35:23 +0530

    view home notification

---
+++ src/app/home-page-components/view-home-notifications/view-home-notifications.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-home-notifications',
+  imports: [],
+  templateUrl: './view-home-notifications.component.html',
+  styleUrl: './view-home-notifications.component.css'
+})
+export class ViewHomeNotificationsComponent {
+
+}
