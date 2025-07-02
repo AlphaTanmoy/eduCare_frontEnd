8b6c95dc2e79ab29dae782c5e891ef0a9e3210b1
alphaTanmoy <dtanmoy169@gmail.com>
2025-07-02 17:48:42 +0530

    other-activity added

---
+++ src/app/page-component/gallary/other-activity/other-activity.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other-activity',
+  imports: [],
+  templateUrl: './other-activity.component.html',
+  styleUrl: './other-activity.component.css'
+})
+export class OtherActivityComponent {
+
+}
