299c2477c281e63d21cb6617c893035ffd8ab63d
alphaTanmoy <dtanmoy169@gmail.com>
2025-07-02 11:43:18 +0530

    base componenet for manage counter added

---
+++ src/app/page-component/admin-panel/manage-counter/manage-counter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-counter',
+  imports: [],
+  templateUrl: './manage-counter.component.html',
+  styleUrl: './manage-counter.component.css'
+})
+export class ManageCounterComponent {
+
+}
