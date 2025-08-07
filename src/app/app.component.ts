68f9a6f60f17ee8882e8e8a768951e70ff407ac1
bhaskar4k <bhaskarndy@gmail.com>
2025-08-07 21:48:57 +0530

    Separated IP-Blocker Component from being render in router-outlet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { VerticalLayoutComponent } from './layout/vertical-layout/vertical-layou
 import { HorizontalLayoutComponent } from './layout/horizontal-layout/horizontal-layout.component';
 import { AuthService } from './service/auth/Auth.Service';
 import { FooterComponent } from './home-page-components/footer/footer.component';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { IpblockerComponent } from './error/ipblocker/ipblocker.component';
+import { loadBootstrap, removeBootstrap } from '../load-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -15,18 +19,23 @@ import { FooterComponent } from './home-page-components/footer/footer.component'
     FontAwesomeModule,
     HorizontalLayoutComponent,
     VerticalLayoutComponent,
-    FooterComponent
+    FooterComponent,
+    IpblockerComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
+  private bootstrapElements!: { css: HTMLLinkElement; js: HTMLScriptElement };
+
   isSmallScreen = window.innerWidth < 1350;
   isLoggedIn = false;
   faExpandArrowsAlt = faExpandArrowsAlt;
   title: any;
 
-  constructor(library: FaIconLibrary, private cdRef: ChangeDetectorRef, private authService: AuthService,) {
+  isOnIpBlockerPage: boolean = false;
+
+  constructor(library: FaIconLibrary, private cdRef: ChangeDetectorRef, private authService: AuthService, private router: Router) {
     library.addIcons(faExpandArrowsAlt);
   }
 
@@ -38,9 +47,25 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     //this.setLayout();
+    this.bootstrapElements = loadBootstrap();
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const segments = event.urlAfterRedirects.split('/').filter(Boolean);
+        const firstRoute = segments[0];
+
+        if (firstRoute == "ip-blocker") {
+          this.isOnIpBlockerPage = true;
+        }
+      });
   }
 
   isAuthenticatedAndLoggedIn(): boolean {
     return this.authService.isUserLoggedIn() && this.authService.isAuthenticated();
+  }
+
+  ngOnDestroy(): void {
+    removeBootstrap(this.bootstrapElements);
   }
 }
