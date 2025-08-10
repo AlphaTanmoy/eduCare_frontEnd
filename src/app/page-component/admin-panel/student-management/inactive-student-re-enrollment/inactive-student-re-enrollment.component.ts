013893dc37f21a9d8ede31fd46694af86c5d77e8
Bhaskar Nandy <bhaskarndy@gmail.com>
2025-08-10 22:08:50 +0530

    Fees refunded student's re-enrollment control added

---
+++ src/app/page-component/admin-panel/student-management/inactive-student-re-enrollment/inactive-student-re-enrollment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inactive-student-re-enrollment',
+  imports: [],
+  templateUrl: './inactive-student-re-enrollment.component.html',
+  styleUrl: './inactive-student-re-enrollment.component.css'
+})
+export class InactiveStudentReEnrollmentComponent {
+
+}
