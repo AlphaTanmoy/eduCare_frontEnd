01e0c0e8e2042da0698a1ecc107554abbb99fe5b
Bhaskar Nandy <bhaskarndy@gmail.com>
2025-08-12 00:40:37 +0530

    Updated env

--- src/app/endpoints/endpoints.ts
+++ src/app/endpoints/endpoints.ts
@@ -4,8 +4,8 @@ export const EndpointType = {
 }
 
 export function GetBaseURL() {
-  // return EndpointType.dev;
-  return EndpointType.prod;
+  return EndpointType.dev;
+  // return EndpointType.prod;
 }
 
 export const Endpoints = {
